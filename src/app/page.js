03a80634616e6495ee670818120cf56028fb58a5
DRobinson <drobinson@benchmarkeducation.com>
2024-03-18 18:07:52 -0400

    complete homepage

--- src/app/page.js
+++ src/app/page.js
@@ -29,8 +29,21 @@ export default function Home() {
             team optimizes delivery routes with algorithms, ensuring your
             NARDT experience is fast and efficient, one slice at a time.
           </p>
+          <a href="#">dkd</a>
         </div>
       </section>
+      <section>
+       
+      </section>
+      <section className="text-center my-8">
+      <SectionHeaders subHeader={'Don\'t hesitate'} mainHeader={'Contact us'} />
+      <div className="mt-8">
+      <a className="text-4xl underline text-gray-500" href="#">212 EAT DATA</a>
+      </div>
+      </section>
+      <footer className="border-t p-8 text-center to-gray-500 mt-16">
+        &copy; 2024 All rights reserved  
+      </footer>
     </>
   );
 }
